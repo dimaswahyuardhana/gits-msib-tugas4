71deb969931e24fd78257577423b2364f4db5020
dimaswahyuardhana <dimasardhana29@gmail.com>
2023-03-08 11:51:49 +0700

    update view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,42 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    @foreach ($data as $mhs)
-        <p>nama : {{ $mhs->nama }}</p>
-        <p>alamat : {{ $mhs->alamat }}</p>
-    @endforeach
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>DATA MAHASISWA UDINUS</h1>
+    <div class="table-responsive md-9">
+        <table class="table table-striped table-hover table-bordered table-primary border-dark">
+          <thead>
+            <tr>
+              <th scope="col">NO</th>
+              <th scope="col">NAMA</th>
+              <th scope="col">NIM</th>
+              <th scope="col">TTL</th>
+              <th scope="col">GENDER</th>
+              <th scope="col">ALAMAT</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($data as $mhs)
+            <tr>
+                <td>{{ $mhs->id }}</td>
+                <td>{{ $mhs->nama }}</td>
+                <td>{{ $mhs->nim }}</td>
+                <td>{{ $mhs->ttl }}</td>
+                <td>{{ $mhs->gender }}</td>
+                <td>{{ $mhs->alamat }}</td>
+              </tr>
+            @endforeach
+
+
+          </tbody>
+
+        </table>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
 </html>
