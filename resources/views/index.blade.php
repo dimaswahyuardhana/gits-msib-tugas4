eb75f7eb01b600df6ad8de7cb4b9fad41d82eb4e
dimaswahyuardhana <dimasardhana29@gmail.com>
2023-03-08 20:10:48 +0700

    update view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
   <body>
-    <h1>DATA MAHASISWA UDINUS</h1>
-    <div class="table-responsive md-9">
+    <h1 align="center">DATA MAHASISWA UDINUS</h1>
+    <div class="table-responsive">
         <table class="table table-striped table-hover table-bordered table-primary border-dark">
           <thead>
             <tr>
